ad0740c0e1c2633b8578309a2824b804d2ed03d5
Georgi Matsov <georgi.macov@gmail.com>
2015-05-13 13:07:26 +0300

    Added Homework 02 Advanced C#/2. Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries

---
+++ Advanced C#/2. Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries/01.FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class FillTheMatrix
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+
+        PaternA(n, matrix);
+        Console.WriteLine();
+        PaternB(n, matrix);
+    }
+
+    private static void PaternA(int n, int[,] matrix)
+    {
+        for (int row = 0; row < n; row++)
+        {
+            for (int column = 0; column < n; column++)
+            {
+                matrix[row, column] = row + 1 + column * 4;
+                Console.Write("{0,4}", matrix[row, column]);
+            }
+            Console.WriteLine();
+        }
+    }
+    private static void PaternB(int n, int[,] matrix)
+    {
+        for (int row = n-1; row >=0; row--)
+        {
+            for (int column = 0; column < n; column++)
+            {
+                if (column% 2==0)
+                {
+                    matrix[row, column] = column*4 - row;
+                }
+                else
+                {
+                    //not enought time
+                }
+                Console.Write("{0,4}", matrix[row, column]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Advanced C#/2. Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries/04.CountSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+class CountSymbols
+{
+    private static void Main()
+    {
+        string input = Console.ReadLine();
+        SortedDictionary<char, int> letters = new SortedDictionary<char, int>();
+
+        foreach (char symbol  in input)
+        {
+            if (letters.ContainsKey(symbol))
+            {
+                letters[symbol]++;
+            }
+            else
+            {
+                letters.Add(symbol,1);
+            }
+        }
+
+        foreach (KeyValuePair<char, int> pair in letters)
+        {
+            Console.WriteLine("{0} -> {1} time/s",pair.Key,pair.Value);
+        }
+    }
+}
