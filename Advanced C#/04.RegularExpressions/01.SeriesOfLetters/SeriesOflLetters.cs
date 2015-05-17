0f776bbd984841663babf2fcb2ae900566627d39
Georgi Matsov <georgi.macov@gmail.com>
2015-05-17 21:56:06 +0300

    Added homework 04 RegularExpressions

---
+++ Advanced C#/04.RegularExpressions/01.SeriesOfLetters/SeriesOflLetters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class SeriesOfLetters
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        string removedLetters = Regex.Replace(input, @"(\w)\1+", "$1");
+        Console.WriteLine(removedLetters);
+    }
+}
---
+++ Advanced C#/04.RegularExpressions/02.ReplaceLinkTag/ReplaceLinkTag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ReplaceLinkTag
+{
+    static void Main()
+    {
+        string input = @"
+                <ul>
+                 <li>
+                  <a href=http://softuni.bg>SoftUni</a>
+                 </li>
+                </ul>";
+        string patern = @"<a\s+href=([^>]+)>([^<]+)<\/a";
+        string replaceTag = "[URL href=$1]$2[/URL]";
+        string output = Regex.Replace(input, patern, replaceTag);
+        Console.WriteLine(output);
+    }
+}
