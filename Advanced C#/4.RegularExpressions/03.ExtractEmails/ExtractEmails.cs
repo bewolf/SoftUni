a96c1a80cad22b7a9f10835509c57779df1f3eb7
Georgi Matsov <georgi.macov@gmail.com>
2015-05-20 11:16:26 +0300

    Rename main folder of Regular Expressions homework Advanced C#

---
+++ Advanced C#/4.RegularExpressions/03.ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Configuration;
+using System.Text.RegularExpressions;
+
+class ExtractEmails
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string mailPatern = @"\b([A-Za-z0-9]+?)[\w\-\.]*?[A-Za-z0-9]+?@[A-Za-z0-9]+?([\w\-\.]+)\2*?\.[\w]{2,}\b";
+        Regex regex = new Regex(mailPatern);
+        MatchCollection mails = regex.Matches(input);
+        foreach (var mail in mails)
+        {
+            Console.WriteLine("{0}",mail);
+        }
+    }
+}
---
+++ Advanced C#/4.RegularExpressions/04.SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class SentenceExtractor
+{
+    static void Main()
+    {
+        string keyword = Console.ReadLine();
+        string inputText = Console.ReadLine();
+
+        MatchCollection sentences = Regex.Matches(inputText, @"(\b.*?\b" + keyword + @"\b.*?[.?!])");
+
+        foreach (var sentence in sentences)
+        {
+            Console.WriteLine(sentence);
+        }
+    }
+}
