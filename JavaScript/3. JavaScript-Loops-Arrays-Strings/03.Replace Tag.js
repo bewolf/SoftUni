b4baeb5dc57c1ab8b499184c16d0660aead76e49
Georgi Matsov <georgi.macov@gmail.com>
2016-01-14 21:19:54 +0200

    big fixed

--- JavaScript/3. JavaScript-Loops-Arrays-Strings/03.Replace Tag.js
+++ JavaScript/3. JavaScript-Loops-Arrays-Strings/03.Replace Tag.js
@@ -2,6 +2,7 @@ function replaceATag() {
     var element = document.getElementsByTagName('a')[0].outerHTML;
     console.log(element);
     element = element.replace('<a', '[URL').replace('</a>', '[/URL]');
+    element = element.replace(new RegExp('>'), ']');
     console.log(element);
     document.getElementsByTagName('li')[0].innerHTML = element;
 }
