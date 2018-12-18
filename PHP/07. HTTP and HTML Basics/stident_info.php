6468cf6b31e8d2f7a04e949790da23317bba8493
bewolf <georgi.macov@mail.bg>
2018-12-18 22:14:48 +0200

    modify student_info_frontend.php and stident_info.php

--- PHP/07. HTTP and HTML Basics/stident_info.php
+++ PHP/07. HTTP and HTML Basics/stident_info.php
@@ -1,7 +1,18 @@
 <?php
-$names = $_POST['names'];
-$ages = $_POST['ages'];
-$delimeter = $_POST['delimeter'];
 
+if (isset($_POST['filter'])) {
+    $names = $_POST['names'];
+    $ages = $_POST['ages'];
+    $delimeter = $_POST['delimeter'];
+    $studentNames = explode($delimeter, $names);
+    $studentAges = (explode($delimeter, $ages));
 
-include ("student_info_frontend.php");
+    for ($i = 0; $i < count($studentAges); $i++) {
+        if ($studentAges[$i] < 18) {
+            unset($studentAges[$i]);
+            unset($studentNames[$i]);
+        }
+    }
+
+}
+include("student_info_frontend.php");
--- PHP/07. HTTP and HTML Basics/student_info_frontend.php
+++ PHP/07. HTTP and HTML Basics/student_info_frontend.php
@@ -5,36 +5,43 @@
     <title>Student Info</title>
 </head>
 <body>
-<div>
-    Delimeter:
-    <select name="delimeter" id="">
-        <option value=",">,</option>
-        <option value="|">|</option>
-        <option value="&">&</option>
-    </select>
-</div>
-<div>
-    Names:
-    <input type="text" name="names">
-</div>
-<div>
-    Ages:
-    <input type="text" name="ages">
-</div>
-<div>
-    <input type="submit" name="filter" value="Filter">
-</div>
-<?php
-if (isset($names, $ages)):?>
-    <table border="1" cellpadding="0">
-        <thead>
-        <th>Name</th>
-        <th>Age</th>
-        </thead>
-        <tbody>
-
-        </tbody>
-    </table>
-<?php endif ?>
+<form method="post">
+    <div>
+        Delimeter:
+        <select name="delimeter" id="">
+            <option value=",">,</option>
+            <option value="|">|</option>
+            <option value="&">&</option>
+        </select>
+    </div>
+    <div>
+        Names:
+        <input type="text" name="names">
+    </div>
+    <div>
+        Ages:
+        <input type="text" name="ages">
+    </div>
+    <div>
+        <input type="submit" name="filter" value="Filter">
+    </div>
+    <?php
+    if (isset($studentNames, $studentAges)):?>
+        <table border="1" cellpadding="0">
+            <thead>
+            <th>Name</th>
+            <th>Age</th>
+            </thead>
+            <tbody>
+            <?php for ($i = 0; $i < count($studentNames); $i++): ?>
+            <tr>
+                <td><?= $studentNames[$i]; ?> </td>
+                <td><?= $studentAges[$i]; ?></td>
+            </tr>
+            <?php endfor; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
+</form>
 </body>
 </html>
