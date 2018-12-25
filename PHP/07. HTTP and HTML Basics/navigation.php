41d609a8dd4cb7d263aac59bde6442f63a13420c
bewolf <georgi.macov@mail.bg>
2018-12-25 10:00:16 +0200

    modify navigation.php

--- PHP/07. HTTP and HTML Basics/navigation.php
+++ PHP/07. HTTP and HTML Basics/navigation.php
@@ -1,11 +1,67 @@
+<?php
+
+if (isset($_GET['tags']) && isset($_GET['months']) && isset($_GET['categories'])) {
+    $tags = explode(', ', $_GET['tags']);
+    $months = explode(', ', $_GET['months']);
+    $categories = explode(', ', $_GET['categories']);
+
+}
+?>
 <form>
-    categories: <input type="text" name="categories"><br><br>
-    tags: <input type="text" name="tags"><br><br>
-    months: <input type="text" name="months"><br><br>
+    Categories: <input type="text" name="categories"><br><br>
+    Tags: <input type="text" name="tags"><br><br>
+    Months: <input type="text" name="months"><br><br>
 
     <input type="submit"/>
 
+    <table>
+        <tbody>
+        <tr>
+            <th>Categories</th>
+
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <?php foreach ($categories as $value): ?>
+
+                        <li><?php echo $value; ?></li>
+
+                    <?php endforeach; ?>
+                </ul>
+            </td>
+        </tr>
+
+        <tr>
+            <th>Tags</th>
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <?php foreach ($tags as $value): ?>
+
+                        <li><?php echo $value; ?></li>
+
+                    <?php endforeach; ?>
+                </ul>
+            </td>
+        </tr>
+
+        <tr>
+            <th>Months</th>
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <?php foreach ($months as $value): ?>
+
+                        <li><?php echo $value; ?></li>
+
+                    <?php endforeach; ?>
+                </ul>
+            </td>
+        </tr>
+
+        </tbody>
+    </table>
 </form>
-
-<?php
-
